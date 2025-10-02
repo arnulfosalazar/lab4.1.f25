e16d0be6aa4485661f52ad9892efe6d065f50a59
arnulfosalazar <arnulfosalazar2001@gmail.com>
2025-10-01 20:20:21 -0500

    Added double and string datatypes to overload swap function

--- lab4.1.cpp
+++ lab4.1.cpp
@@ -5,19 +5,58 @@ using namespace std;
 
 //function prototyping
 void swap(int& a, int& b);
+void swap(double& c, double& d);
+void swap(string& e, string& f);
 
 //declaring variables to eventually swap
 int main() {
 	int a = 7;
 	int b = 6;
+	double c = 7.69;
+	double d = 6.67;
+	string e = "World";
+	string f = "Hello";
+	
+	//output for users and swaps
 	cout << "a is: " << a << " and b is: " << b << endl;
 
 	swap(a, b);
 	cout << "a is now: " << a << " and b is now: " << b << endl;
+	
+	cout << "c is: " << c << " and d is: " << d << endl;
+
+	swap(c, d);
+	cout << "c is now: " << c << " and d is now: " << d << endl;
+
+	cout << "e is: " << e << " and f is: " << f << endl;
+
+	swap(e, f);
+	cout << "e is now: " << e << " and f is now: " << f << endl;
+
+
 }
 
+//function for ints
 void swap(int& a, int& b) {
 	int a1 = a;
 	a = b;
 	b = a1;
+}
+
+//function for doubles
+void swap(double& c, double& d) {
+	double c1 = c;
+	c = d;
+	d = c1;
+
+
+}
+
+//function for strings
+void swap(string& e, string& f) {
+	string e1 = e;
+	e = f;
+	f = e1;
+
+
 }
